bf03ef094cea04e11e957c42945c1d95c1988cc1
itskwame <168389950+itskwame@users.noreply.github.com>
2026-01-04 14:33:57 -0500

    Updated package-lock.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "11df816ccdf12e3b"; }
+/******/ 		__webpack_require__.h = function() { return "f59e6f3d24f34dbf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
